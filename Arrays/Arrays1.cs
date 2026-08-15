8c1be45dd6429da6c077fc5a55d9c44f67c3063a
noella <noellamarizbeltran01@gmail.com>
2026-08-15 19:49:54 +0800

    Update and rename Array1cs to Arrays1.cs

---
+++ Arrays/Arrays1.cs
@@ -0,0 +1,14 @@
+Console.Write("Enter the size of the array:");
+
+int size = int.Parse(Console.ReadLine());
+
+int[] myArray = new int[size];
+
+Console.WriteLine("Enter the elements of the array:");
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Element {i + 1}: ");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nOriginal array:" + string.Join(", ", myArray));
