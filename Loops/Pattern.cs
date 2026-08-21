ccaa0214773fae8abf06fe931de1f4a9d2340f78
noella <noellamarizbeltran01@gmail.com>
2026-08-21 21:04:10 +0800

    Add right triangle pattern in Pattern.cs
    Implement a right triangle pattern using nested loops.

---
+++ Loops/Pattern.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Right Triangle Pattern:");
+
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(" ");
