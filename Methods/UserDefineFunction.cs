f97f3d1da1dfb92d1e831a1bd57218b3f9eabec7
noella <noellamarizbeltran01@gmail.com>
2026-08-21 21:06:06 +0800

    Add UserDefineFunction with welcome message

---
+++ Methods/UserDefineFunction.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace UserDefineFunction
+{
+    public class Program
+    {
+        static void UDF()
+        {
+            Console.WriteLine("Welcome Friends!");
+            Console.WriteLine("Have a nice day!");
+        }
+        static void Main()
+        {
+            UDF();
+        }
+    }
+}
